6dc813be5996602d1398e6a889ac7e813137183b
Pralish Kayastha <pralishkayastha@gmail.com>
2025-07-15 19:13:54 -0400

    handle cors

--- rga_chatapp/settings.py
+++ rga_chatapp/settings.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = DJANGO_APPS + OTHER_APPS
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -64,6 +65,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'rga_chatapp.urls'
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 TEMPLATES = [
     {
